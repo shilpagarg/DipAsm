0a784bdfdc99da8e159bd4fdc99bc4149b185c04
EC2 Default User <ec2-user@ip-172-31-26-139.us-east-2.compute.internal>
2020-02-12 23:53:17 +0000

    minor

--- INSTALLATION.sh
+++ INSTALLATION.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 # installation of tools not on conda
+
+
 [ -d tools ] || mkdir -p tools
 cd tools
-git clone https://github.com/malonge/RaGOO.git
-cd RaGOO
-python setup.py install
-cd ../
+
+git clone https://github.com/cschin/Peregrine.git
+cd Peregrine
+bash install_with_conda.sh
+cd ..
+
+conda activate peregrine
+conda deactivate
 
 wget https://github.com/broadinstitute/picard/releases/download/2.20.2/picard.jar
 
-git clone https://github.com/vibansal/HapCUT2.git
-cd HapCUT2
-make
-cd ../
+git clone https://github.com/shilpagarg/HapCUT2.git
+wget https://github.com/gt1/biobambam2/releases/download/2.0.87-release-20180301132713/biobambam2-2.0.87-release-20180301132713-x86_64-etch-linux-gnu.tar.gz
 
-conda env create -f environment.yml
+tar -xvzf biobambam2-2.0.87-release-20180301132713-x86_64-etch-linux-gnu.tar.gz
 
-if ( ! sudo yum install -y docker ) ; then
-echo \"sudo yum install -y docker\" failed, trying apt-get;
-if ( ! sudo apt-get install --yes docker ); then
-echo \"sudo apt-get install --yes docker\" failed.;
-echo docker installation failed;
-echo If you have docker intalled, run \"sudo systemctl start docker\" to activate it.
-exit;
-fi
-fi
+cd ..
 
-sudo systemctl start docker
+conda env create -n whdenovo -f environment.yml
+conda activate whdenovo
+conda deactivate
+
+conda env create -n dv -f dv.yml
+conda activate dv
+conda deactivate
+
 
