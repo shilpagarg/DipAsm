f84bd698f231ad19009d386e90c5bb365eef11b8
Heng Li <lh3@me.com>
2020-03-20 20:42:24 -0400

    Install hapcut2 through conda

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 mkdir -p hi-c; cp tests/hic_* hi-c/
 mkdir -p pacbioccs; cp tests/hifi.fastq pacbioccs/
+conda activate whdenovo
 python pipeline.py --hic-path hi-c --pb-path pacbioccs --sample test --prefix out
