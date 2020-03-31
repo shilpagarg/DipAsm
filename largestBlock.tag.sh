6a4de35c8e25d209d0f1d1a3759e0a2b14d25b67
Shilpa <shilpa.garg2k7@gmail.com>
2020-03-30 20:50:00 -0400

    clean up

--- largestBlock.tag.sh
+++
@@ -1,8 +0,0 @@
-CHR=$1
-PS=`grep -v '^#' phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.vcf  | grep 'PS' | cut -d':' -f13 | sort | uniq -cd | sort -k1nr | head -1 | awk '{print $2}'`
-grep -E "^#|$PS$" phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.vcf   > phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.largestBlock.vcf 
-bgzip -c phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.largestBlock.vcf > phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.largestBlock.vcf.gz
-tabix -p vcf phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.largestBlock.vcf.gz
-mkdir taggedBAM
-whatshap haplotag --reference phasedVCF/GRCh37.fasta phasedVCF/pacbioccs.hic.${CHR}.whatshap.phased.largestBlock.vcf.gz pacbioccs/pacbioccs.${CHR}.bam -o taggedBAM/na12878.pacbioccs.${CHR}.largestBlock.tagged.bam
-
