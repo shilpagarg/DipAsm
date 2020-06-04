5fc0f1219fcb2383b618096e953277762b747119
Jason Chin <cschin@infoecho.net>
2020-06-04 21:31:05 +0000

    make the docker workdir in the current directory ($PWD), so a user does not need root permission to create a work dir. Running Docker will still need the root to set it for a system

--- dv.sh
+++ dv.sh
@@ -1,5 +1,5 @@
-INPUT_DIR="$1"
-OUTPUT_DIR="$1/alignment/pacbioccs/vcf"
+INPUT_DIR="$HOSTWD/test_output/out/"
+OUTPUT_DIR="$HOSTWD/test_output/out/alignment/pacbioccs/vcf"
 BIN_VERSION="0.8.0"
 SCAFF=$2
 PAT=`echo $SCAFF | sed 's/\([^\\]\);/\1\\\\;/g' | sed 's/\([^\\]\)=/\1\\\\=/g'`
@@ -7,6 +7,8 @@ echo $PAT
 
 #short=`echo $SCAFF | cut -d';' -f1 | cut -d'.' -f2`
 #echo $short
+echo INPUT DIR: ${INPUT_DIR}
+echo OUTDIR DIR: ${OUTPUT_DIR}
 docker run \
   -v "${INPUT_DIR}":"/input" \
   -v "${OUTPUT_DIR}":"/output" \
